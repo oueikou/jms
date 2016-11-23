246bed27d67f81a0217d6c24ba77dcc4f6582923
yongheng.wang <yongheng@yongheng-PC>
2016-11-23 17:27:57 +0800

    加日志

--- src/main/java/com/jms/activemq/listener/ConsumerSessionAwareMessageListener.java
+++ src/main/java/com/jms/activemq/listener/ConsumerSessionAwareMessageListener.java
@@ -1,5 +1,7 @@
 package com.jms.activemq.listener;
 
+import org.apache.log4j.Logger;
+
 import javax.jms.JMSException;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -8,10 +10,24 @@ import org.apache.activemq.Message;
 import org.springframework.jms.listener.SessionAwareMessageListener;
 
 public class ConsumerSessionAwareMessageListener implements SessionAwareMessageListener<Message>{
+	/**
+	* Logger for this class
+	*/
+	private static final Logger logger = Logger.getLogger(ConsumerSessionAwareMessageListener.class);
 
 	public void onMessage(Message message, Session session) throws JMSException {
-		TextMessage textMsg = (TextMessage) message;
-		System.out.println("消息内容是：" + textMsg.getText());
+		if (logger.isDebugEnabled()) {
+			logger.debug("onMessage(Message, Session) - start");
+		}
+
+		if (message instanceof TextMessage) {
+			TextMessage textMsg = (TextMessage) message;
+			logger.info("消息内容是：" + textMsg.getText());
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("onMessage(Message, Session) - end");
+		}
 	}
 
 }
